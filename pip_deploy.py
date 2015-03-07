acea3c11fc74c8d9b4ad46e0df1bb9300d810757
Matt Carlson <mattcarlson@allionusa.com>
2015-03-06 16:51:03 -0800

    MAde pip_deploy a shell script instead of a python script and made it executable. It should run in BASH and Windows shell.

--- pip_deploy.py
+++
@@ -1,4 +0,0 @@
-import subprocess
-
-subprocess.call('python setup.py sdist')
-subprocess.call('python setup.py sdist bdist_wheel upload')
---
+++ pip_deploy.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+
+python setup.py sdist
+python setup.py sdist bdist_wheel upload
